67e63336e0c4976874dac1e6af488e945b142bb1
jatin831 <jatinbajaj2001@gmail.com>
2021-10-30 17:57:55 +0530

    transaction API

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema({
+        description: {
+            type: String,
+            required: true
+        },
+        groupName: {
+            type: String,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        paidBy: {
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        friends: [Schema.Types.ObjectId],
         
     },
     {
