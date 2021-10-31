ef0d8c10093a01eb85b95fdb0120d2ce18945200
jatin831 <jatinbajaj2001@gmail.com>
2021-10-31 19:16:02 +0530

    created transaction history cards

--- client/src/components/Dashboard/dashboard.jsx
+++ client/src/components/Dashboard/dashboard.jsx
@@ -7,6 +7,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import AddFriendModal from '../AddFriendModal/addFriend';
 import { useSelector } from "react-redux";
 import { selectUserData } from '../../reduxSlices/authSlice';
+import { getDateStringFromTimestamp, getTimeFromTimestamp } from "../../utilities";
 
 const Dashboard = () => {
   const userData = useSelector(selectUserData);
@@ -15,6 +16,7 @@ const Dashboard = () => {
   const [friends, setFriends] = useState([]);
   const [show, setShow] = useState(false);
   const toggle = () => setShow(prevState=>!prevState);
+  const [transactionStatus, setTransactionStatus] = useState();
 
   useEffect(() => {
     setLoading(true);
@@ -50,6 +52,19 @@ const Dashboard = () => {
             alert(err.response.data.message);
         }
       })
+
+    axios.get("http://localhost:5000/user/getTransactionStatus", {
+      params: {
+        // userId: userData.userId,
+        userId: "617cc0b8b998f18d6814c439"
+      }
+    })
+    .then(res => {
+      setTransactionStatus(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }, []);
 
   return (
@@ -97,19 +112,19 @@ const Dashboard = () => {
               <div className="d-flex justify-content-around col-12 pt-3 pb-2 containerWhite">
                 <div className="d-flex flex-column align-items-center">
                   <h3 className="mb-0">
-                    <h4 className="d-inline">&#x20B9;</h4> 00.00
+                    <h4 className="d-inline">&#x20B9;</h4> {transactionStatus.netBalance.toFixed(2)}
                   </h3>
                   Net Balance
                 </div>
                 <div className="d-flex flex-column align-items-center">
                   <h3 className="mb-0">
-                    <h4 className="d-inline">&#x20B9;</h4> 00.00
+                    <h4 className="d-inline">&#x20B9;</h4> {transactionStatus.credit.toFixed(2)}
                   </h3>
                   Credit Amount
                 </div>
                 <div className="d-flex flex-column align-items-center">
                   <h3 className="mb-0">
-                    <h4 className="d-inline">&#x20B9;</h4> 00.00
+                    <h4 className="d-inline">&#x20B9;</h4> {transactionStatus.debit.toFixed(2)}
                   </h3>
                   Debit Amount
                 </div>
@@ -128,10 +143,85 @@ const Dashboard = () => {
                   {transactions.length === 0 ? (
                     <h5>No Transactions...</h5>
                   ) : (
-                    transactions.map((transactions) => {
+                    transactions.map((transaction) => {
+                      let youOwe, whoPaid;
+                      let flag = 0; // youOwe
+                      if (transaction._id === userData.userId) {
+                        flag = 1; // youPaid
+                      } else {
+                        transaction.friends.forEach(friend => {
+                          console.log(friend);
+                          console.log(userData);
+                          if (friend.id === userData.userId || friend.id === "617cc0b8b998f18d6814c439") {
+                            youOwe = -friend.debt;
+                            whoPaid = friend.name;
+                          }
+                        })
+                      }
+
                       return (
-                        <div className="d-flex Transaction">
-                          ka
+                        <div className="d-flex Transaction justify-content-between">
+                          <div className="d-flex">
+                            <div className="d-flex flex-column">
+                              <div>
+                                <h4 className="m-0 p-0">{transaction.description}</h4>
+                              </div>
+                              <div className="m-0 p-0">
+                                <span className="fs-6">Group:</span><h6 className="d-inline m-0 p-0"> {transaction.groupName}</h6>
+                              </div>
+                            </div>
+                            <div className="d-flex flex-column ms-5 align-items-center">
+                              <div className="fs-5">
+                                {getDateStringFromTimestamp(transaction.createdAt)}
+                              </div>
+                              <div className="fs-6 fw-bold">
+                                {getTimeFromTimestamp(transaction.createdAt)}
+                              </div>
+                            </div>
+                          </div>
+                          <div className ="d-flex justify-content-end">
+                            {
+                              flag ? (
+                                <>
+                                  <div className="d-flex flex-column mx-3 align-items-center">
+                                    <div className="TransactionAmount">
+                                      {transaction.amount}
+                                    </div>
+                                    <div className="TransactionText">
+                                      You Paid
+                                    </div>
+                                  </div>
+                                  <div className="d-flex flex-column mx-3 me-5 align-items-center">
+                                    <div className="TransactionAmount">
+                                      {transaction.amount}
+                                    </div>
+                                    <div className="TransactionText">
+                                      You Paid
+                                    </div>
+                                  </div>
+                                </>
+                              ) : (
+                                <div className="d-flex">
+                                  <div className="d-flex flex-column mx-3 align-items-center">
+                                    <div className="TransactionAmount">
+                                      {youOwe}
+                                    </div>
+                                    <div className="TransactionText">
+                                      {whoPaid} Paid
+                                    </div>
+                                  </div>
+                                  <div className="d-flex flex-column mx-3 me-5 align-items-center">
+                                    <div className="TransactionAmount">
+                                      {youOwe}
+                                    </div>
+                                    <div className="TransactionText">
+                                      {whoPaid} Lent You
+                                    </div>
+                                  </div>
+                                </div>
+                              )
+                            }
+                          </div>
                         </div>
                       );
                     })
