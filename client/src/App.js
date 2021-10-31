3be9a1cd834d79f48766d235016e04cb54ac7c8e
jatin831 <jatinbajaj2001@gmail.com>
2021-10-31 11:37:41 +0530

    updated app.js client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './components/home';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Dashboard from './components/Header/dashboard';
 import { useEffect } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -7,40 +8,45 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { AUTOLOGIN } from './reduxSlices/authSlice';
+import { Switch, Route } from 'react-redux-dom';
 
 const App =() => {
   const dispatch = useDispatch();
+  // const userData = useSelector()
 
   useEffect(()=>{
     dispatch(AUTOLOGIN());
 
-    axios.get("http://localhost:5000/user/getFriends",{
-      params:{
-        userId:"617ce19ca08f2465fa748cbf"
-      }
-    }).then(res=>{
-      console.log(res);
-    })
-    .catch(err=>{
-      console.log(err);
-    })
+    // axios.get("http://localhost:5000/user/getFriends",{
+    //   params:{
+    //     userId:"617ce19ca08f2465fa748cbf"
+    //   }
+    // }).then(res=>{
+    //   console.log(res);
+    // })
+    // .catch(err=>{
+    //   console.log(err);
+    // })
 
-    axios.post("http://localhost:5000/user/addFriend",{
-      friendsEmail:"jatinbajaj2001@gmail.com",
-      userId: "617ce19ca08f2465fa748cbf"
-    }).then(res=>{
-      console.log(res);
-    })
-    .catch(err=>{
-      console.log(err);
-    })
+    // axios.post("http://localhost:5000/user/addFriend",{
+    //   friendsEmail:"jatinbajaj2001@gmail.com",
+    //   userId: "617ce19ca08f2465fa748cbf"
+    // }).then(res=>{
+    //   console.log(res);
+    // })
+    // .catch(err=>{
+    //   console.log(err);
+    // })
   })
 
   return (
    <>
-   <BrowserRouter>
-    <Dashboard/>
-    </BrowserRouter>
+    {
+      
+    }
+    <Switch>
+      <Dashboard/>
+    </Switch>
    </> 
   )
 }
